445cc3e5a236a0acc3d37bc1169530bf0e4a6154
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-12-23 00:12:43 +0700

    enable session, compression, https

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var https = require('https');
+var path = require('path');
+var sslConfig = require(path.join(__dirname,'ssl_config.js'));
 
 var app = module.exports = loopback();
 
@@ -7,17 +10,47 @@ var app = module.exports = loopback();
 // var PassportConfigurator =
 require('loopback-component-passport').PassportConfigurator;
 
-app.start = function() {
+app.use(loopback.session({
+    secret: 'secret session',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(loopback.compress());
+
+app.start = function(httpOnly) {
+
+  if (httpOnly === undefined) {
+    httpOnly = process.env.HTTP;
+  }
+
+  var server=null;
+
+  if(httpOnly) {
+    server = http.createServer(app);
+
+  } else {
+    var options = {
+      key: sslConfig.privateKey,
+      cert: sslConfig.certificate
+    };
+    server = https.createServer(options, app);
+  }
+
+
   // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+  server.listen(app.get('port'),function() {
+    var baseUrl = (httpOnly? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
+    app.emit('started', baseUrl);
+    console.log('Web server listening at: %s/', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
+
+  return server;
+
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
