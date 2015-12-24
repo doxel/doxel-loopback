0f130253ac0a2ac33a702d3fa0a8d6a3fd83a30f
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-12-25 04:56:15 +0700

    disable connect-redis for passport twitter auth

--- server/boot/session.js
+++ server/boot/session.js
@@ -1,10 +1,10 @@
 // https://github.com/strongloop/loopback/issues/1032
 module.exports = function(app) {
   var session = require('express-session');
-  var RedisStore = require('connect-redis')(session);
-  var store = new RedisStore({ host: '127.0.0.1' });
+//  var RedisStore = require('connect-redis')(session);
+//  var store = new RedisStore({ host: '127.0.0.1' });
   app.middleware('session', session({
-    "store": store,
+//    "store": store,
     "saveUninitialized": true,
     "resave": true,
     "secret": "keyboard cat"
