316037cdd0c77d8f8db75f7fd3588bf82cf7ba6c
Rurik Bogdanov <rurik.bugdanov@github.com>
2016-11-09 18:09:19 +0100

    renamed script

---
+++ server/boot/000-passport.js
@@ -0,0 +1,68 @@
+/*
+ * passport.js
+ *
+ * Copyright (c) 2015-2016 ALSENET SA - http://doxel.org
+ * Please read <http://doxel.org/license> for more information.
+ *
+ * Author(s):
+ *
+ *      Luc Deschenaux <luc.deschenaux@freesurf.ch>
+ *
+ * This file is part of the DOXEL project <http://doxel.org>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://doxel.org/license>.
+ */
+
+ module.exports = function(app) {
+  var PassportConfigurator = app.PassportConfigurator; //require('loopback-component-passport').PassportConfigurator;
+  var passportConfigurator = new PassportConfigurator(app);
+  var config = {};
+
+  try {
+    config = require('../providers.json');
+
+  } catch(e) {
+    console.error('Please configure your passport strategy in `providers.json`.');
+    console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+    process.exit(1);
+
+  }
+
+  passportConfigurator.init();
+
+  var flash=require('express-flash');
+  app.use(flash());
+
+  passportConfigurator.setupModels({
+    userModel: app.models.user,
+    userIdentityModel: app.models.userIdentity,
+    userCredentialModel: app.models.userCredential
+  });
+
+  for (var strategy in config) {
+    var cfg=config[strategy];
+    cfg.session=(cfg.session!==false);
+    passportConfigurator.configureProvider(strategy,cfg);
+  }
+
+}
