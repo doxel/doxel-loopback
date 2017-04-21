fa79d37e54f05f4611d64097ff975b238d66f2e2
Rurik Bogdanov <rurik.bugdanov@github.com>
2017-04-21 17:33:44 +0200

    move maintenance scripts

---
+++ server/scripts/segment-cloud.js
@@ -0,0 +1,53 @@
+module.exports=function(app){
+  /*
+   * import cloud data in database:
+   *
+   * - create pointCloud instance
+   * - update segment picture
+  */
+
+  var Q=require('q');
+
+  var Segment=app.models.Segment;
+  var Pose=app.models.Pose;
+  var PointCloud=app.models.PointCloud;
+
+  Q.fcall(function(){
+    // delete all PointCloud and Pose instances
+    return Q(PointCloud.destroyAll()).then(Q(Pose.destroyAll()));
+
+  })
+  .then(function(){
+   // get all the segment ids
+   return Q(Segment.find({
+     fields: ['id']
+   }))
+
+  })
+  .then(function(segments){
+    var segment_idx=0;
+
+    function segments_loop(){
+      // exit loop when no segment left
+      if (segment_idx>=segments.length) {
+        console.log('segment-viewer-status-update DONE');
+        return
+      }
+
+      // get next segment
+      var segment=segments[segment_idx++];
+      console.log(segment_idx+'/'+segments.length);
+
+      // create segment PointCloud and Pose instances
+      Segment._injectPointcloud(segment.id)
+      .fail(console.log)
+      .finally(segments_loop)
+      .done();
+    }
+
+    // enter the loop
+    segments_loop();
+
+  });
+}
+
