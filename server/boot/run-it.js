35cf6e926ddfefbb8832b2fd352c8dd7338c5250
Rurik Bogdanov <rurik.bugdanov@github.com>
2017-04-21 17:27:15 +0200

    run-it.js: allow to run maintenance scripts from command line

---
+++ server/boot/run-it.js
@@ -0,0 +1,14 @@
+module.exports=function(app){
+
+  // Allow to run maintenance scripts from command line
+  // eg: PORT=4444 node . $(realpath updatedb.js)
+  // 
+
+  if (process.argv[1].split('/').pop()!='doxel-loopback' || process.argv.length<3) {
+    return;
+  }
+
+  require(process.argv[2])(app);
+
+}
+
