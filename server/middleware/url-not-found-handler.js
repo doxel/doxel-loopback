6da755f2ab09c73338ae01675b32d8b9b8c67b46
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-10-17 14:59:58 +0700

    send 404 in html only when appropriate

--- server/middleware/url-not-found-handler.js
+++ server/middleware/url-not-found-handler.js
@@ -3,11 +3,18 @@ var path=require('path');
 module.exports = function () {
     //4XX - URLs not found
     return function customRaiseUrlNotFoundError(req, res, next) {
-        res.sendFile(path.join(__dirname,'404.html'), function (err) {
-            if (err) {
-                console.error(err);
-                res.status(err.status).end();
-            }
-        });
+        // send html only when acceptable
+        if (req.headers.accept && req.headers.accept.match(/html/)) {
+          res.sendFile(path.join(__dirname,'404.html'), function (err) {
+              if (err) {
+                  console.error(err);
+                  res.status(err.status).end();
+              }
+          });
+
+        } else {
+          res.status(404).end();
+          return;
+        }
     };
 };
